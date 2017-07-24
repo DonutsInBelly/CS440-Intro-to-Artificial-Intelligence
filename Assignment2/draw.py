67b504b67f5aa970c1c257dda3ea97c1ee2e6750
Carlin Au <carlinau@gmail.com>
2017-07-23 21:50:17 -0400

    added more things from assign2.py it now finds a goal and sets it as self.n and self.m and assigns colors

--- Assignment2/draw.py
+++ Assignment2/draw.py
@@ -1,6 +1,6 @@
 import sys
 import random
-import pygame
+import pygame as pygame
 
 cell_size = 100
 
@@ -21,13 +21,14 @@ class Board(object):
         self.width = width
         self.height = height
         self.cells = {} # A dictionary of tuples, for each will be a list of values
+        # Defines each cell in self.cells
         for x in range(width):
             for y in range(height):
                 self.cells[(x, y)] = {'state': None,     # None, Flat, Hilly, Forest, or Caves
-                                 'prob': None,      # Probability
-                                 'is_goal': False,  # Weather or not it is the goal node
-                                 'parent': None}    # Keep track of path
-
+                                     'prob': None,      # Probability
+                                     'is_goal': False,  # Weather or not it is the goal node
+                                     'parent': None}    # Keep track of path
+        # Sets probabilities for each cell in the board
         for x in range(width):
             for y in range(height):
                 p = random.random()
@@ -43,6 +44,14 @@ class Board(object):
                 elif p <= 1.0:
                     self.cells[(x, y)]['state'] = 'Caves'
                     self.cells[(x, y)]['prob'] = 0.1
+        # Picks a random (n,m) to be the Goal
+        self.n = int(width * random.random())
+        self.m = int(height * random.random())
+        for x in range(width):
+            for y in range(height):
+                if x == self.n and y == self.m:
+                    self.cells[(x, y)]['is_goal'] = True
+        # Pygame setup
         pygame.init()
         self.size = width, height = (cell_size * width) + 2, (cell_size * height) + 2
         self.screen = pygame.display.set_mode(self.size)
@@ -58,6 +67,24 @@ class Board(object):
             pygame.draw.line(background, white, (i, 0), (i, cell_size * self.width), 2)
             pygame.draw.line(background, white, (0, i), (cell_size * self.height, i), 2)
         self.background = background
+        # Assigns color to cell depending on terrain type given
+        for x in range(self.width):
+            for y in range(self.height):
+                top = (x * cell_size) + 2
+                left = (y * cell_size) + 2
+                if self.cells[(x, y)]['state'] == 'Flat':
+                    r = pygame.Rect(left, top, cell_size - 2, cell_size - 2)
+                    pygame.draw.rect(self.background, light_gray, r, 0)
+                elif self.cells[(x, y)]['state'] == 'Hilly':
+                    r = pygame.Rect(left, top, cell_size - 2, cell_size - 2)
+                    pygame.draw.rect(self.background, light_green, r, 0)
+                elif self.cells[(x, y)]['state'] == 'Forest':
+                    r = pygame.Rect(left, top, cell_size - 2, cell_size - 2)
+                    pygame.draw.rect(self.background, dark_green, r, 0)
+                elif self.cells[(x, y)]['state'] == 'Caves':
+                    r = pygame.Rect(left, top, cell_size - 2, cell_size - 2)
+                    pygame.draw.rect(self.background, gray, r, 0)
+
 
     # Refresh function
     def show_board(self):
--- Assignment2/main.py
+++ Assignment2/main.py
@@ -2,5 +2,6 @@ import draw as draw
 
 board = draw.Board(10,10)
 board.init_board()
+
 while True:
     board.show_board()
