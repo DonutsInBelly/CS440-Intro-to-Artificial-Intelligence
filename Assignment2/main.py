552562898db32438512771f1730680967a60cafd
Carlin Au <carlinau@gmail.com>
2017-07-26 10:23:04 -0400

    Removed unnecessary work inside main.py

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -8,12 +8,4 @@ board.inorderSearch()
 x = 0
 y = 0
 while True:
-    board.tries += 1
-    if x > board.width:
-        x = 0
-        y = 0
-    roll = rn.random()
-    if roll <= board.cells[(x, y)]['prob']:
-        if board.cells[(x, y)]['is_goal']:
-            break
     board.show_board()
