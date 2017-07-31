db9229ee2ecaabb25ea4a9cc2a1fcd39d68c1b24
Carlin Au <carlinau@gmail.com>
2017-07-31 16:41:16 -0400

    Rule 1 done

--- Assignment2/draw.py
+++ Assignment2/draw.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import pygame as pygame
+import numpy as np
 
 cell_size = 15
 
@@ -25,7 +26,7 @@ class Board(object):
         self.height = height
         self.tries = 0
         self.cells = {} # A dictionary of tuples, for each will be a list of values
-        
+
         # Defines each cell in self.cells
         for x in range(width):
             for y in range(height):
@@ -33,23 +34,23 @@ class Board(object):
                                      'prob': None,      # Probability
                                      'is_goal': False,  # Weather or not it is the goal node
                                      'chance_of_goal': None}    # Keep track of path
-        
+
         # Sets probabilities for each cell in the board
         for x in range(width):
             for y in range(height):
                 p = random.random()
                 if p <= 0.25:
                     self.cells[(x, y)]['state'] = 'Flat'
-                    self.cells[(x, y)]['prob'] = 0.8
+                    self.cells[(x, y)]['prob'] = np.float64(0.2)
                 elif p <= 0.5:
                     self.cells[(x, y)]['state'] = 'Hilly'
-                    self.cells[(x, y)]['prob'] = 0.6
+                    self.cells[(x, y)]['prob'] = np.float64(0.4)
                 elif p <= 0.75:
                     self.cells[(x, y)]['state'] = 'Forest'
-                    self.cells[(x, y)]['prob'] = 0.4
+                    self.cells[(x, y)]['prob'] = np.float64(0.6)
                 elif p <= 1.0:
                     self.cells[(x, y)]['state'] = 'Caves'
-                    self.cells[(x, y)]['prob'] = 0.1
+                    self.cells[(x, y)]['prob'] = np.float64(0.9)
 
         # Picks a random (n,m) to be the Goal
         self.n = int(width * random.random())
@@ -112,7 +113,66 @@ class Board(object):
             for y in range(self.height):
                 self.prob[x][y] = 1.0/(self.width*self.height)
 
-    
+    def rule1(self):
+        belief = {}
+        # Initialize belief states
+        for x in range(self.width):
+            for y in range(self.height):
+                belief[(x,y)] = float(1)/(self.width * self.height)
+                # belief[(x, y)] = random.random()
+        # Start Search
+        tries = 0
+        found_goal = False
+        curx = 0
+        cury = 0
+        while found_goal == False:
+            # Search cell
+            tries += 1
+            roll = random.random()
+            checkedBt = belief[(x, y)]
+            checkedProb = self.cells[(curx, cury)]['prob']
+            print("Try: " + str(tries) + ", Checking " + str(curx) + ", " + str(cury) + " with probability: " + str(belief[curx,cury]) + "; Goal at: (" + str(self.n) + ", " + str(self.m) + ")")
+            # print(tries)
+            if roll <= checkedProb:
+                if self.cells[(curx, cury)]['is_goal']:
+                    found_goal = True
+                    break
+            # Update belief states and pick new curx and cury
+            newx = 0
+            newy = 0
+            curBt = belief[(0,0)]
+            if self.cells[(0,0)]['is_goal']:
+                belief[(0,0)] = np.float64(checkedBt * checkedProb)/((checkedBt * checkedProb) + (1 - checkedBt))
+                # belief[(0,0)] = np.float64(checkedBt * checkedProb)/((belief[(0,0)] * self.cells[(0,0)]['prob']) + (1 - checkedBt))
+            else:
+                belief[(0,0)] = np.float64(curBt)/((checkedBt * checkedProb) + (1 - checkedBt))
+                # belief[(0,0)] = np.float64(curBt)/((belief[(0,0)] * self.cells[(0,0)]['prob']) + (1 - checkedBt))
+            for x in range(self.width):
+                for y in range(self.height):
+                    curBt = belief[(x,y)]
+                    curProb = self.cells[(x,y)]['prob']
+                    if x == curx and y == cury:
+                        # if x == self.n and y == self.m:
+                        #     belief[(x,y)] = np.float64((checkedBt * checkedProb))/((checkedBt * checkedProb) + (1 - checkedBt))
+                        # else:
+                        #     # belief[(x,y)] = np.float64(curBt)/((checkedBt * checkedProb) + (1 - checkedBt))
+                        belief[(x,y)] = np.float64((checkedBt * checkedProb))/((checkedBt * checkedProb) + (1 - checkedBt))
+                        # belief[(x,y)] = np.float64(checkedBt * checkedProb)/((belief[(x,y)] * self.cells[(x,y)]['prob']) + (1 - checkedBt))
+                    else:
+                        belief[(x,y)] = np.float64(curBt)/((checkedBt * checkedProb) + (1 - checkedBt))
+                        # belief[(x,y)] = np.float64(curBt)/((belief[(x,y)] * self.cells[(x,y)]['prob']) + (1 - checkedBt))
+                    # print("New Belief of (" + str(x) + ", " + str(y) + "): " + str(belief[(x,y)]))
+            for x in range(self.width):
+                for y in range(self.height):
+                    if belief[(newx, newy)] < belief[(x,y)]:
+                        # print("New x and y")
+                        newx = x
+                        newy = y
+            # print(belief)
+            curx = newx
+            cury = newy
+        print("Finished with : " + str(tries))
+        return tries
 
     def inorderSearch(self):
         self.tries = 0
@@ -184,11 +244,11 @@ class Board(object):
             else:
                 self.prob_prev = self.prob[x][y]
                 self.prob[x][y] = self.prob[x][y]*((1-self.cells[(x, y)]['prob'])/(1-self.prob[x][y]))  # Bayes' Theorem
-                # self.prob[x][y] = self.prob[x][y]*(1-self.cells[(x, y)]['prob'])/((self.prob[x][y]*(1-self.cells[(x, y)]['prob']))+(1-self.prob[x][y])) # Bayes' Theorem, with the denom expanded     
+                # self.prob[x][y] = self.prob[x][y]*(1-self.cells[(x, y)]['prob'])/((self.prob[x][y]*(1-self.cells[(x, y)]['prob']))+(1-self.prob[x][y])) # Bayes' Theorem, with the denom expanded
                 # UNCOMMENT ABOVE IF YOU WANT TO BE A LITTLE MORE CORRECT... (TIME CONSUMING)
                 self.normalize(x, y)
                 x = self.x
-                y = self.y 
+                y = self.y
                 # print(str(self.tries) + ": " + str(self.prob_max))
         print(self.tries)
 
@@ -216,6 +276,6 @@ class Board(object):
                 self.prob[x][y] = self.prob[x][y] * self.cells[(x, y)]['prob']  # Here, we scale the probability by the cell's likelihood of finding the goal, thereby prioritizing environments where finding the target is more likely
                 self.normalize(x, y)
                 x = self.x
-                y = self.y 
+                y = self.y
                 # print(str(self.tries) + ": " + str(self.prob_max))
         print(self.tries)
--- Assignment2/main.py
+++ Assignment2/main.py
@@ -8,9 +8,17 @@ board.show_board()
 x = 0
 y = 0
 
-board.inorderSearch()
-board.baye_1()
-board.baye_2()
+# board.inorderSearch()
+# board.baye_1()
+# board.baye_2()
+# board.rule1()
+total = 0
+for i in range(15):
+	cur = board.rule1()
+	total += cur
+	board = draw.Board(50,50)
 
-while True:
-	board.show_board()
+avg = total / float(15)
+print("Final Average = " + str(avg))
+# while True:
+# 	board.show_board()
