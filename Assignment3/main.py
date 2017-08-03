c7185c3174a92e4e467498a1c7610d127e314274
Carlin Au <carlinau@gmail.com>
2017-08-03 16:52:13 -0400

    Actual grayscaling of an image without colormap cheating

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -10,9 +10,38 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+    gray = np.ndarray((len(rgb),len(rgb[0])))
+    for x in range(len(rgb)):
+        for y in range(len(rgb[x])):
+            code = int((rgb[(x,y,0)] * 0.21) + (rgb[(x,y,1)] * 0.72) + (rgb[(x,y,2)] * 0.07))
+            arr = [code, code, code]
+            gray[(x,y)] = int((rgb[(x,y,0)] * 0.21) + (rgb[(x,y,1)] * 0.72) + (rgb[(x,y,2)] * 0.07))
+            print(gray[(x,y)])
+    return gray
 
-img = mpimg.imread('image1.jpg')
+
+img = mpimg.imread('profile.jpg')
 gray = rgb2gray(img)
-plt.imshow(gray, cmap = plt.get_cmap('gray'))
+model = {}
+newColor = 0
+seenColor = 0
+# for i in range(256):
+# for gx in range(len(gray)):
+#     for gy in range(len(gray[gx])):
+#         if gray[(gx,gy)] in model.keys():
+#             seenColor += 1
+#         else:
+#             model[gray[(gx,gy)]] = 'lol'
+#             newColor += 1
+#             # colorR
+#             # colorG
+#             # colorB
+#             # model[i] = color
+
+# print(gray[(0,1)])
+# print(img[(0,0,1)])
+print(newColor)
+print(seenColor)
+plt.gray()
+plt.imshow(gray)
 plt.show()
