022089d6fde236d0ac2b993e36847cbde76f4f4a
Carlin Au <carlinau@gmail.com>
2017-08-04 09:27:48 -0400

    more cleanup

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-# import numpy as np
-# import scipy.misc as sp
-#
-# print(sp.__dict__.keys())
-# # im = scipy.misc.imread("image1.jpg")
-# # print(im)
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -17,15 +12,12 @@ def rgb2gray(rgb):
             print(gray[(x,y)])
     return gray
 
-
 img = mpimg.imread('profile.jpg')
 gray = rgb2gray(img)
 model = {}
 newColor = 0
 seenColor = 0
 
-print(newColor)
-print(seenColor)
 plt.gray()
 plt.imshow(gray)
 plt.show()
